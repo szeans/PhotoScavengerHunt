4d19296c4ba020164d30ed1e42b755bb1321f1b8
Sean Choi <szeanchoi@gmail.com>
2023-02-08 23:36:22 -0500

    done

---
+++ PhotoScavengerHunt/Task List/ThingCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThingCell.swift
+//  PhotoScavengerHunt
+//
+//  Created by SEAN CHOI on 2/8/23.
+//
+
+import UIKit
+
+class ThingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
